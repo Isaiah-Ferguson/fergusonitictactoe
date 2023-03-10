f34acaba7830e4f25438199c2f8424f896bff79d
Isaiah-Ferguson <crestice@yahoo.com>
2023-03-10 14:25:13 -0800

    Full Game Commit

---
+++ src/components/boardcomponents/BoardComponent.js
@@ -0,0 +1,85 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import Square from "../squarecomponent/SquareComponent";
+import calculateWinner from "../calculatewinnercomponent/CalculateWinnerComponent";
+
+let count = 0;
+let countTwo = 0;
+export default function Board({ xIsNext, squares, onPlay }) {
+
+    function handleClick(i) {
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      const nextSquares = squares.slice();
+      if (xIsNext) {
+        nextSquares[i] = 'X';
+      } else {
+        nextSquares[i] = 'O';
+      }
+      onPlay(nextSquares);
+    }
+    calculateWinner(squares);
+    console.log(count)
+    const winner = calculateWinner(squares);
+    let status;
+    let playerOne;
+    if (winner) {
+        if(winner === "X"){
+            count++;
+            
+            if (count % 2 === 0) {
+                const halfCount = count / 2;
+                playerOne = 'Player X is on a  ' + halfCount + " Game Win Streak";
+                status = 'Winner: Player X';
+                countTwo=0;
+              }
+            
+        }else{
+            countTwo++;
+            if (countTwo % 2 === 0) {
+                const halfCountTwo = countTwo / 2;
+                playerOne = 'Player O is on a ' + halfCountTwo + " Game Win Streak"
+                status = 'Winner: O';
+                count = 0;
+              }
+            
+        }
+    } else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
+
+
+
+    return (
+      <div>
+        
+        <Row>
+            <Col className="col-12">
+            
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+            </Col>
+        </Row>
+        <div className="status">{playerOne}</div>
+        <div className="status">{status}</div>
+      </div>
+    );
+  }
+
---
+++ src/components/gamecomponent/GameComponent.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Board from "../boardcomponents/BoardComponent";
+import { Row, Col, Button } from "react-bootstrap";
+import { useState } from "react";
+
+export default function Game() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+        
+    }
+    console.log("hi")
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move;
+        } else {
+            description = 'Go to game start';
+        }
+        return (
+            <li style={{ paddingTop: 14 }} key={move}>
+                <button className="algoia" onClick={() => jumpTo(move)}>{description}</button>
+
+            </li>
+        );
+    });
+
+    return (
+
+        <div className="bg container-fluid">
+            <br></br>
+            <div className="text-center ">
+                <p className="titleText">Tic Tac Toe</p>
+            </div>
+
+
+
+            <Row>
+                <Col className="col-12 d-flex justify-content-center">
+                    <div className="game-board">
+                        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+                    </div>
+                    <div className="game-info">
+                        <div>{moves}</div>
+                    </div>
+                </Col></Row>
+
+        </div>
+    );
+}
+
